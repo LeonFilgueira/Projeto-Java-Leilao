b79e1638e1731f11700a2c59b9fc883f309e38eb
Leon Filgueira <periclesleon.filgueira@gmail.com>
2024-07-15 15:44:57 -0300

    Listagem de produtos funcionando

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -12,7 +12,9 @@ import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ProdutosDAO {
 
@@ -27,8 +29,8 @@ public class ProdutosDAO {
         prep = null;
         try {
             prep = conn.prepareStatement("INSERT INTO produtos(nome, valor) VALUES(?, ?)");
-            prep.setString(1,produto.getNome());
-            prep.setInt(2,produto.getValor());
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
             prep.executeUpdate();
         } catch (SQLException ex) {
             System.out.println("Erro ao conectar: " + ex.getMessage());
@@ -45,7 +47,26 @@ public class ProdutosDAO {
     }
 
     public ArrayList<ProdutosDTO> listarProdutos() {
+        conn = new conectaDAO().connectDB();
+        ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+        String sql = "SELECT * FROM produtos";
+        prep = null;
+        try {
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery(sql);
+            
 
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+                listagem.add(produto);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Erro: " + ex.getMessage());
+        }
         return listagem;
     }
 
